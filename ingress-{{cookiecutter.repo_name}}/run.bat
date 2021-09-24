cf774c89b3d997ec50e54f799ef97ca40408f11e
Nasib <TheBurningDude@users.noreply.github.com>
2021-09-24 14:41:02 +0200

    Renaming

--- ingress-{{cookiecutter.repo_name}}/run.bat
+++ ingress-{{cookiecutter.repo_name}}/run.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-CALL E:\ingress-adapter-{{cookiecutter.repo_name}}\venv\Scripts\activate.bat
+CALL E:\ingress-{{cookiecutter.repo_name}}\venv\Scripts\activate.bat
 PUSHD E:
-CD E:\ingress-adapter-{{cookiecutter.repo_name}}
-CALL python -m ingress_adapter_{{cookiecutter.module_name}}.adapter
-CALL E:\ingress-adapter-{{cookiecutter.repo_name}}\venv\Scripts\deactivate.bat
+CD E:\ingress-{{cookiecutter.repo_name}}
+CALL python -m ingress_{{cookiecutter.module_name}}.adapter
+CALL E:\ingress-{{cookiecutter.repo_name}}\venv\Scripts\deactivate.bat
