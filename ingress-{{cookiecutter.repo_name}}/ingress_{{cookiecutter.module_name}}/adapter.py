6a4cc8aec75e4b5f349360941fbe8763d598a162
Jens Rix <jensrix@gmail.com>
2021-11-17 13:27:32 +0000

    Changed time interval variable names

--- ingress-{{cookiecutter.repo_name}}/ingress_{{cookiecutter.module_name}}/adapter.py
+++ ingress-{{cookiecutter.repo_name}}/ingress_{{cookiecutter.module_name}}/adapter.py
@@ -47,46 +47,46 @@ def main():
                           dataset_guid=config['Datasets']['source'])
 
     max_interval_to_retrieve = pd.Timedelta(config.max_interval_to_retrieve)
-    run_adapter_based_on_state_file = args.start_time is not None
+    run_adapter_based_on_state_file = args.from_date is not None
     if run_adapter_based_on_state_file:
-        start_time, end_time = extract_time_interval_from_state_file(state)
-        next_start_time = retrieve_and_upload_data(start_time, end_time)
+        from_date, to_date = extract_time_interval_from_state_file(state)
+        next_from_date = retrieve_and_upload_data(from_date, to_date)
 
-        state['next_start_time'] = datetime.strftime(next_start_time, DATE_FORMAT)
+        state['next_from_date'] = datetime.strftime(next_from_date, DATE_FORMAT)
         state['last_successful_run'] = datetime.strftime(datetime.utcnow(), DATE_FORMAT)
         ingress_api.save_state(state)
     else:
-        start_time, end_time = args.start_time, args.end_time
-        retrieve_and_upload_data(start_time, end_time)
+        from_date, to_date = args.from_date, args.to_date
+        retrieve_and_upload_data(from_date, to_date)
 
 
-def retrieve_and_upload_data(start_time, end_time, max_interval_to_retrieve: timedelta):
+def retrieve_and_upload_data(from_date, to_date, max_interval_to_retrieve: timedelta):
     """
     Retrieves and uploads data to Ingress. If the input time interval is longer than max_interval_to_retrieve,
     it splits the interval and makes multiple retrievals and uploads.
     """
 
-    if end_time - start_time <= max_interval_to_retrieve:
-        retrieved_data, next_start_time = retrieve_data(start_time, end_time)
+    if to_date - from_date <= max_interval_to_retrieve:
+        retrieved_data, next_from_date = retrieve_data(from_date, to_date)
         upload_data_to_ingress(ingress_api, retrieved_data)
     else:
-        retrieve_start_time = start_time
-        retrieve_end_time = min(end_time, retrieve_start_time + max_interval_to_retrieve)
+        retrieve_from_date = from_date
+        retrieve_to_date = min(to_date, retrieve_from_date + max_interval_to_retrieve)
 
-        while retrieve_start_time < end_time:
-            retrieved_data, retrieve_start_time = retrieve_data(retrieve_start_time, retrieve_end_time)
+        while retrieve_from_date < to_date:
+            retrieved_data, retrieve_from_date = retrieve_data(retrieve_from_date, retrieve_to_date)
             upload_data_to_ingress(ingress_api, retrieved_data)
 
-            retrieve_end_time = min(end_time, retrieve_start_time + max_interval_to_retrieve)
+            retrieve_to_date = min(to_date, retrieve_from_date + max_interval_to_retrieve)
 
             # TODO_TEMPLATE: Make sure that the loop breaks and logs error if the request is the same again and again.
 
-        next_start_time = retrieve_start_time
+        next_from_date = retrieve_from_date
 
-    return next_start_time
+    return next_from_date
 
 
-def retrieve_data(start_time: datetime, end_time: datetime) -> Tuple([List(Dict(str))]):
+def retrieve_data(from_date: datetime, to_date: datetime) -> Tuple([List(Dict(str))]):
     """
     Retrieves data from the source and stores it in a dataframe. The actual end time of the data is extracted and
     returned together with Ingress filename(s) and dataframe(s).
@@ -96,25 +96,25 @@ def retrieve_data(start_time: datetime, end_time: datetime) -> Tuple([List(Dict(
     dataframe = pd.read_csv('https://data.dk/csv_data', parse_dates=['timestamp'])
 
     # TODO: Extract actual data end time. Example:
-    data_end_time = dataframe.timestamp.max() + timedelta(hours=1)
-    filename = _get_filename(start_time, data_end_time, time_format='%Y%m%dT%H')
+    data_end_date = dataframe.timestamp.max() + timedelta(hours=1)
+    filename = _get_filename(from_date, data_end_date, time_format='%Y%m%dT%H')
 
     # If you need to retrieve data from e.g. multiple endpoints, you can do something like:
     # data = []
-    # data_end_times = []
+    # data_end_dates = []
     # for url in urls:
     #     dataframe = pd.read_csv(url)
 
-    #     data_end_time = dataframe.timestamp.max() + timedelta(hours=1)
-    #     filename = _get_filename(start_time, data_end_time, time_format='%Y%m%dT%H')
+    #     data_end_date = dataframe.timestamp.max() + timedelta(hours=1)
+    #     filename = _get_filename(from_date, data_end_date, time_format='%Y%m%dT%H')
 
     #     data.append({filename, dataframe})
 
-    # data_end_time = min(data_end_times)
+    # data_end_date = min(data_end_dates)
 
-    # return data, data_end_time
+    # return data, data_end_date
 
-    return [{filename: dataframe}], data_end_time
+    return [{filename: dataframe}], data_end_date
 
 
 def upload_data_to_ingress(ingress_api, retrieved_data):
@@ -130,22 +130,22 @@ def upload_data_to_ingress(ingress_api, retrieved_data):
         logger.info(f'Data successfully uploaded: {file_name}')
 
 
-def _get_filename(start_time, end_time, time_format='%Y%m%dT%H%M%SZ'):
+def _get_filename(from_date, to_date, time_format='%Y%m%dT%H%M%SZ'):
     """Generates filename (without file extension) as time interval."""
-    start_time_str = datetime.strftime(start_time, time_format)
-    end_time_str = datetime.strftime(end_time, time_format)
-    return f'{start_time_str}--{end_time_str}'
+    from_date_str = datetime.strftime(from_date, time_format)
+    to_date_str = datetime.strftime(to_date, time_format)
+    return f'{from_date_str}--{to_date_str}'
 
 
 def extract_time_interval_from_state_file(state):
     """
-    Generates start_time and end_time based on state file.
+    Generates from_date and to_date based on state file.
     """
     # TODO: Rewrite this so that it fits to your project
-    start_time = datetime.strptime(state['next_start_time'], DATE_FORMAT)
-    end_time = datetime.utcnow()
+    from_date = datetime.strptime(state['next_from_date'], DATE_FORMAT)
+    to_date = datetime.utcnow()
 
-    return start_time, end_time
+    return from_date, to_date
 
 
 def __init_argparse() -> argparse.ArgumentParser:
@@ -159,11 +159,11 @@ def __init_argparse() -> argparse.ArgumentParser:
                         nargs='+',
                         default=['credentials.ini', '/vault/secrets/credentials.ini'],
                         help='setting the credential file')
-    parser.add_argument('--start_time',
+    parser.add_argument('--from_date',
                         nargs=1,
                         type=lambda s: parse_date_str(s),
                         help=f'setting the start time for the adapter.')
-    parser.add_argument('--end_time',
+    parser.add_argument('--to_date',
                         nargs=1,
                         type=lambda s: parse_date_str(s),
                         default=datetime.utcnow(),
