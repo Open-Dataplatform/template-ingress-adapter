8763fa89b6d203266d2c446ffe87db0722adfc3b
jensrix <jensrix@gmail.com>
2022-01-03 11:50:47 +0000

    Deleted run.bat

--- ingress-{{cookiecutter.repo_name}}/run.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-CALL E:\ingress-{{cookiecutter.repo_name}}\venv\Scripts\activate.bat
-PUSHD E:
-CD E:\ingress-{{cookiecutter.repo_name}}
-CALL python -m ingress_{{cookiecutter.module_name}}
-CALL E:\ingress-{{cookiecutter.repo_name}}\venv\Scripts\deactivate.bat
